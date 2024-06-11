c5e599c5a11901686809070f01381e48d58f76ef
Aidan James <ajame23@uwo.ca>
2024-06-11 17:21:52 -0400

    clicking working

--- src/Editing.js
+++ src/Editing.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import "./Editing.css"
 
 export default function Editing() {
-const [words, setWords] = useState([Array(12).fill(null)])
+const [words, setWords] = useState([Array(16).fill(null)])
   const [inputText, setInput] = useState("")
 
   function handleClick(i) {
@@ -10,8 +11,8 @@ const [words, setWords] = useState([Array(12).fill(null)])
   }
 
   return (
-    <div className="App">
-      <div className='headingBar'>
+    <div className="Editing">
+      <div className='heading-bar'>
         <header>Connections</header>
       </div>
 
@@ -22,27 +23,27 @@ const [words, setWords] = useState([Array(12).fill(null)])
         >
         </input>
         <b>{inputText}</b>
-        <div className='input-row'>
-          <ConnectButton key={0} value={words[0]} onConnectClick={() => handleClick(0)}/>
-          <ConnectButton key={1} value={words[1]} onConnectClick={() => handleClick(1)}/>
-          <ConnectButton key={2} value={words[2]} onConnectClick={() => handleClick(2)}/>
-          <ConnectButton key={3} value={words[3]} onConnectClick={() => handleClick(3)}/>
-        </div>
-        <div className='input-row'>
-          <ConnectButton key={4} value={words[4]} onConnectClick={() => handleClick(4)}/>
-          <ConnectButton key={5} value={words[5]} onConnectClick={() => handleClick(5)}/>
-          <ConnectButton key={6} value={words[6]} onConnectClick={() => handleClick(6)}/>
-          <ConnectButton key={7} value={words[7]} onConnectClick={() => handleClick(7)}/>
-        </div>
-        <div className='input-row'>
-          <ConnectButton key={8} value={words[8]} onConnectClick={() => handleClick(8)}/>
-          <ConnectButton key={9} value={words[9]} onConnectClick={() => handleClick(9)}/>
-          <ConnectButton key={10} value={words[10]} onConnectClick={() => handleClick(10)}/>
-          <ConnectButton key={11} value={words[11]} onConnectClick={() => handleClick(11)}/>
+        <div className='game-area'>
+          <ConnectButton value={words[0]} onConnectClick={() => handleClick(0)}/>
+          <ConnectButton value={words[1]} onConnectClick={() => handleClick(1)}/>
+          <ConnectButton value={words[2]} onConnectClick={() => handleClick(2)}/>
+          <ConnectButton value={words[3]} onConnectClick={() => handleClick(3)}/>
+          <ConnectButton value={words[4]} onConnectClick={() => handleClick(4)}/>
+          <ConnectButton value={words[5]} onConnectClick={() => handleClick(5)}/>
+          <ConnectButton value={words[6]} onConnectClick={() => handleClick(6)}/>
+          <ConnectButton value={words[7]} onConnectClick={() => handleClick(7)}/>
+          <ConnectButton value={words[8]} onConnectClick={() => handleClick(8)}/>
+          <ConnectButton value={words[9]} onConnectClick={() => handleClick(9)}/>
+          <ConnectButton value={words[10]} onConnectClick={() => handleClick(10)}/>
+          <ConnectButton value={words[11]} onConnectClick={() => handleClick(11)}/>
+          <ConnectButton value={words[12]} onConnectClick={() => handleClick(8)}/>
+          <ConnectButton value={words[13]} onConnectClick={() => handleClick(9)}/>
+          <ConnectButton value={words[14]} onConnectClick={() => handleClick(10)}/>
+          <ConnectButton value={words[15]} onConnectClick={() => handleClick(11)}/>
         </div>
       </div>
 
-      <div className='footer'>
+      <div className='bottom-buttons'>
         <button>Create</button>
       </div>
     </div>
--- src/Playing.js
+++ src/Playing.js
@@ -1,12 +1,94 @@
 import './Playing.css';
 import React, { useState } from 'react';
 
-export default function Playing() {
-  const one = [(1, 'one'), (1, 'two'), (1, 'three'), (1, 'four')]
-  const two = [(2, 'apple'), (2, 'banana'), (2, 'grape'), (2, 'pear')]
-  const three = [(3, 'shirt'), (3, 'pants'), (3, 'socks'), (3, 'underwear')]
-  const four = [(4, 'CPU'), (4, 'GPU'), (4, 'RAM'), (4, 'Motherboard')]
+export default function Playing({ input, one, two, three, four }) {
+  let count = 0; // used for keys
+  const words = input.map(str => [str, false, count++]) // adding false and key to words array
+  const [lives, setLives] = useState(4) // hook for amount of lives player has remaining
+  const [selected, setSelected] = useState([Array(4)]) // hook for boxes used has selected
+  const [numSelected, setNumSelected] = useState(0) // hook for how may boxes uses has selected
 
+  // to change but working
+  function lifeHandle() {
+    setLives(lives - 1)
+  }
+  // checking if arrays match
+  function checkArray(user, arr) {
+    const sortedUser = user.slice().sort();
+    const sortedArr = arr.slice().sort();
+    // looping
+    for (let i = 0; i < sortedUser.length; i++) {
+      if (sortedUser[i] !== sortedArr[i]) {
+        return false;
+      }
+    }
+    return true; // arrays are the same
+  }
+  // on submit click
+  function handleSubmit() {
+    if(checkArray(selected, one)) {
+      // one correct
+      console.log("ONE TITLE")
+    } else if (checkArray(selected, two)) {
+      // two correct
+      console.log("TWO TITLE")
+    } else if (checkArray(selected, three)) {
+      // three correct
+      console.log("THREE TITLE")
+    } else if (checkArray(selected, four)) {
+      // four correct
+      console.log("FOUR TITLE")
+    } else {
+      lifeHandle()
+      console.log("not quite")
+    }
+  }
+  // checking if submition is possible
+  function possible() {
+    if(numSelected === 4) {
+      return 'yes'
+    } else {
+      return 'no'
+    }
+  }
+  const p = possible() // called each update for button change
+  // deselecting all selected connect boxes
+  function handleDeselect() {
+    setNumSelected(0)
+    const t = Array(4)
+    setSelected(t)
+
+    console.log("-- deselected --")
+  }
+  // handling click of connect box
+  function handleClick(value) {
+    if(numSelected < 4) {
+      // adding to selected array
+      const t = selected
+      t[numSelected] = value
+      setSelected(t)
+
+      // increment numSelected
+      setNumSelected(numSelected+1)
+      updateME()
+    }
+  }
+  // to be deleted
+  function updateME() {
+    console.log(selected)
+  }
+
+  // NEED TO MAKE IT SO EACH RELOAD WILL NOT SHUFFLE ARRAY
+  // const shuffleArray = (array) => {
+  //   const shuffledArray = array.slice(); // Create a copy of the array
+  //   for (let i = shuffledArray.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  //   }
+  //   return shuffledArray;
+  // };
+
+  // what is to be displayed  
   return (
     <div className="Playing">
       <div className='heading-bar'>
@@ -14,81 +96,87 @@ export default function Playing() {
       </div>
 
       <div className='game'>
-        <GameArea first={one} second={two} third={three} fourth={four} />
+        <GameArea words={words} handleClickGame={handleClick} />
       </div>
 
       <div className='footer'>
-        <LivesLeft/>
-        <BottomButtons possible={'yes'}/>
+        <LivesLeft lives={lives}/>
+        <div className='bottom-buttons'>
+          <BottomButtons handleLifeLoss={lifeHandle} handleDeselect={handleDeselect} />
+          <SubmitButton possible={p} handleSubmit={handleSubmit} />
+        </div>
       </div>
     </div>
   );
 }
 
-// game area of app
-function GameArea({ first, second, third, fourth }) {
+// game area of app, returns 4 rows of 4 connect buttons
+function GameArea({ words, handleClickGame }) {
   return (
     <div className='game-area'>
-      <div className='game-row'>
-        {first.map((value) => (
-          <ConnectButton value={value}/>
-        ))}
-      </div>
-      <div className='game-row'>
-        {second.map((value) => (
-          <ConnectButton value={value}/>
-        ))}
-      </div>
-      <div className='game-row'>
-        {third.map((value) => (
-          <ConnectButton value={value}/>
-        ))}
-      </div>
-      <div className='game-row'>
-        {fourth.map((value) => (
-          <ConnectButton value={value}/>
-        ))}
-      </div>
+      {words.map((value, select ) => (
+        <ConnectButton value={value[0]} handleClickConnect={handleClickGame} select={select} key={value[2]}/>
+      ))}
     </div>
   )
 }
-
 
 // each individual "connection box"
-function ConnectButton({ value }) {
-    return (
-      <button 
-        className='connectionBox'
-      >
-        {value}
-      </button>
-    )
+function ConnectButton({ value, handleClickConnect, select}) {
+  return (
+    <button 
+      className='connect-button'
+      onClick={() => handleClickConnect(value)}
+    >
+      {value}
+    </button>
+  )
 }
 
-function LivesLeft() {
-    return (
-      <div className='livesLeft'>
-        <b>Lives Remaining:</b>
-        <Heart/>
-        <Heart/>
-        <Heart/>
-        <Heart/>
-      </div>
-    )
+// returns lives remaining text amount of hearts according to lives prop
+function LivesLeft({ lives }) {
+  const loopFunc = []
+  for(let i = 0; i < lives; i++) {
+    loopFunc.push(i)
   }
-
-function Heart() {
-    return (
-      <b className='dot'>&hearts;</b>
-    )
-  }
-
-function BottomButtons({ possible }) {
   return (
-    <div className='Bottom-button'>
-      <button>Shuffle</button>
-      <button>Deselect All</button>
-      <button className={possible}>Submit</button>
+    <div className='livesLeft'>
+      <b>Lives Remaining:</b>
+      {/* looping through lives, _ because we dont care about index */}
+      {loopFunc.map((_, index) => (
+        <Heart key={index}/>
+      ))}
     </div>
   )
+}
+
+// returns HTML heart as body tag
+function Heart() {
+  return (
+    <b className='dot'>&hearts;</b>
+  )
+}
+
+// returns shuffle, deselect, and submit buttons
+function BottomButtons({ handleLifeLoss, handleDeselect}) {
+  return (
+    <div className='shuffle-deselect'>
+      <button>Shuffle</button>
+      <button onClick={handleDeselect}>Deselect All</button>
+    </div>
+  )
+}
+
+// returns button depending on if it is possible for user to submit the selected boxes
+// possible can be 'yes' for submittable, else will be deactive
+function SubmitButton({ possible, handleSubmit }) {
+  if(possible === 'yes') {
+    return (
+      <button onClick={handleSubmit} className='submit-active'>Submit</button>
+    );
+  } else {
+    return (
+      <button className='submit-inactive'>Submit</button>
+    );
+  }
 }
